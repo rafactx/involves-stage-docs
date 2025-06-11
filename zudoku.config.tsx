cf2df82adf3ded9432aa9a62522811bc22b2cc69
Rafael Teixeira <rctx@rctx-MacBook-Pro.local>
2025-06-10 22:09:08 -0300

    Initial commit from Create Next App

---
+++ zudoku.config.tsx
@@ -0,0 +1,35 @@
+import type { ZudokuConfig } from "zudoku";
+
+const config: ZudokuConfig = {
+  page: {
+    logo: {
+      src: { light: "/logo-light.svg", dark: "/logo-dark.svg" },
+      alt: "Zudoku",
+      width: "130px",
+    },
+  },
+  topNavigation: [
+    { id: "docs", label: "Documentation" },
+    { id: "api", label: "API Reference" },
+  ],
+  sidebar: {
+    docs: [
+      {
+        type: "category",
+        label: "Overview",
+        items: ["docs/introduction", "docs/example"],
+      },
+    ],
+  },
+  redirects: [{ from: "/", to: "/docs/introduction" }],
+  apis: {
+    type: "file",
+    input: "./apis/openapi.yaml",
+    navigationId: "api",
+  },
+  docs: {
+    files: "/pages/**/*.{md,mdx}",
+  },
+};
+
+export default config;
